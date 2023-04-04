bb9d70f47738ae6f115bacf717400a2836b16f45
Pablo <ppmoya08@gmail.com>
2023-04-04 16:20:50 -0400

    Added vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,16 @@
 import { defineConfig } from 'astro/config';
 import mdx from "@astrojs/mdx";
 import compress from "astro-compress";
+import vercelStatic from '@astrojs/vercel/static';
 
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://pabloprx.tech',
-  integrations: [mdx(), compress(), sitemap()]
+  integrations: [mdx(), compress(), sitemap()],
+  output: 'server',
+  adapter: vercelStatic({
+    analytics: true
+  })
 });
